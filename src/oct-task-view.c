a57b53689cfcd96e4e364efeca406429c6934e92
Tilo Villwock <codemusings@gmail.com>
2017-01-24 20:15:21 +0100

    Tasks are added before the new task row now and can be checked off.

--- src/oct-task-view.c
+++ src/oct-task-view.c
@@ -4,7 +4,10 @@
 #include "oct-task-view.h"
 
 static void
-oct_task_view_new_entry_edited(GtkCellRendererText*, gchar*, gchar*, gpointer);
+oct_task_view_checkbox_toggled(GtkCellRendererToggle*, gchar*, gpointer);
+
+static void
+oct_task_view_title_edited(GtkCellRendererText*, gchar*, gchar*, gpointer);
 
 struct _OctTaskView {
     GtkTreeView parent_instance;
@@ -44,16 +47,20 @@ oct_task_view_init(OctTaskView* self)
     gtk_tree_view_set_model(GTK_TREE_VIEW(self), GTK_TREE_MODEL(store));
     g_object_unref(store);
 
+    GtkCellRenderer* checked_renderer = gtk_cell_renderer_toggle_new();
+    g_signal_connect(checked_renderer, "toggled",
+        G_CALLBACK(oct_task_view_checkbox_toggled), self);
     GtkTreeViewColumn* checked_column;
     checked_column = gtk_tree_view_column_new_with_attributes(
-        "", gtk_cell_renderer_toggle_new(),
+        "", checked_renderer,
+        "activatable", COLUMN_EDITABLE,
         "active", COLUMN_CHECKED,
         "visible", COLUMN_CHECKBOX_VISIBLE, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(self), checked_column);
 
     GtkCellRenderer* title_renderer = gtk_cell_renderer_text_new();
     g_signal_connect(title_renderer, "edited",
-        G_CALLBACK(oct_task_view_new_entry_edited), self);
+        G_CALLBACK(oct_task_view_title_edited), self);
     GtkTreeViewColumn* title_column;
     title_column = gtk_tree_view_column_new_with_attributes(
         "", title_renderer,
@@ -63,20 +70,19 @@ oct_task_view_init(OctTaskView* self)
 }
 
 static void
-oct_task_view_new_entry_edited(GtkCellRendererText* renderer,
-                               gchar*               path_string,
-                               gchar*               new_text,
-                               gpointer             user_data)
+oct_task_view_title_edited(GtkCellRendererText* renderer,
+                           gchar*               path_string,
+                           gchar*               new_text,
+                           gpointer             user_data)
 {
-    GtkListStore* store = GTK_LIST_STORE(
-        gtk_tree_view_get_model(GTK_TREE_VIEW(user_data)));
-    gint num_tasks = gtk_tree_model_iter_n_children(
-        GTK_TREE_MODEL(store), NULL);
+    GtkTreeModel* model = gtk_tree_view_get_model(GTK_TREE_VIEW(user_data));
+    GtkTreeIter iter, new_iter;
+    gtk_tree_model_get_iter_from_string(model, &iter, path_string);
+    gint num_tasks = gtk_tree_model_iter_n_children(model, NULL);
 
     if (atoi(path_string) == num_tasks - 1) {
-        GtkTreeIter iter;
-        gtk_list_store_prepend(store, &iter);
-        gtk_list_store_set(store, &iter,
+        gtk_list_store_insert_before(GTK_LIST_STORE(model), &new_iter, &iter);
+        gtk_list_store_set(GTK_LIST_STORE(model), &new_iter,
             COLUMN_ROWID, num_tasks,
             COLUMN_CHECKED, FALSE,
             COLUMN_TITLE, new_text,
@@ -84,12 +90,25 @@ oct_task_view_new_entry_edited(GtkCellRendererText* renderer,
             COLUMN_EDITABLE, TRUE,
             -1);
     } else {
-        GtkTreeModel* model = gtk_tree_view_get_model(GTK_TREE_VIEW(user_data));
-        GtkTreeIter iter;
-        gtk_tree_model_get_iter_from_string(model, &iter, path_string);
         gtk_list_store_set(GTK_LIST_STORE(model), &iter,
             COLUMN_TITLE, g_strdup(new_text), -1);
     }
+}
+
+static void
+oct_task_view_checkbox_toggled(GtkCellRendererToggle* renderer,
+                               gchar*                 path_string,
+                               gpointer               user_data)
+{
+    GtkTreeModel* model = gtk_tree_view_get_model(GTK_TREE_VIEW(user_data));
+    GtkTreeIter iter;
+    gtk_tree_model_get_iter_from_string(model, &iter, path_string);
+
+    gboolean checked;
+    gtk_tree_model_get(model, &iter, COLUMN_CHECKED, &checked, -1);
+
+    gtk_list_store_set(GTK_LIST_STORE(model), &iter,
+        COLUMN_CHECKED, !checked, -1);
 }
 
 GtkWidget*
